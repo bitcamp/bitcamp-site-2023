e05eeebe56e2fd1c3dc0ff73b64d227e885cc216
Varun Chilukuri <varunch321@gmail.com>
2022-12-21 20:42:58 -0500

    Add basic layout of information section

---
+++ components/Information.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="information">
+    <div class="bc-description">
+      <h2>What is Bitcamp?</h2>
+      {{ textBlurb }}
+    </div>
+    <div class="information-image">
+      <img :src="image" alt="section image" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Information',
+  props: {
+    textBlurb: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style>
+.information {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+
+.bc-description {
+  flex: 1;
+  line-height: 1.5;
+  text-align: center;
+  padding: 0 8rem;
+}
+
+.information-image {
+  flex: 1;
+  text-align: center;
+  padding: 3 0rem;
+}
+
+.information-image img {
+  max-width: 100%;
+  height: auto;
+}
+
+@media (max-width: 768px) {
+  .information {
+    flex-direction: column;
+  }
+  .bc-description {
+    text-align: center;
+    padding: 0 3rem;
+  }
+  .information-image {
+    text-align: center;
+  }
+}
+</style>
