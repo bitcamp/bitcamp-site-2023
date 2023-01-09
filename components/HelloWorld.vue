6d80e63c25c338817be82046354fa28935e04573
Rishabh Misra <rmisra077@gmail.com>
2023-01-09 18:05:12 -0500

    Added Mission/Accomplishment Section

--- components/HelloWorld.vue
+++ components/HelloWorld.vue
@@ -1,5 +1,11 @@
 <template>
   <div>Hello world!</div>
+  <MissionAccomplishments 
+    :Accomplishments="[
+      'this is a test of a long message to see what the div does', 
+      'message to be rendered in the middle', 
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit.']">
+  </MissionAccomplishments>
 </template>
 
 <script setup lang="ts"></script>
