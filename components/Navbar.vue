9be93290f05432279d54d8166e00d692d29caef9
Dhruv Raval <raval.dhruv01@gmail.com>
2023-02-21 23:28:51 -0500

    themed nav bar component

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,109 +1,127 @@
 <template>
-
-    <header>
-        <nav>
-            <div class="non-pages">
-                <div id="logo-container">
-                    <a tag="img" href="/">
-                        <img src="~/public/bitcamp-brand/logos/logotype.png" id="logo-with-text">
-                        <img src="~/public/bitcamp-brand/logos/bitcamp.png" id="logo-image">
-                    </a>
-                </div>
-                <div class="hamburgerContainer">
-                    <div @click="toggleDropdown" class="hamburgerBars">
-                        <div class="bar1"></div>
-                        <div class="bar2"></div>
-                        <div class="bar3"></div>
-                    </div>
-                </div>
-
-            </div>
-
-
-            <ul v-if="showDropdown || bigScreen" class="nav-pages">
-                <li class="page">
-                    <a href="/our-mission">Our Mission</a>
-                </li>
-                <li class="page">
-                    <a href="/tracks">Tracks</a>
-                </li>
-                <li class="dropdown-page">
-                    <a href="/mini-events" class="dropdown-text">Mini Events</a>
-                    <div class="dropdown-elements-container">
-                        <ul class="dropdown-elements">
-                            <li>
-                                <a href="/mini-events/event1">Value</a>
-                            </li>
-                            <li>
-                                <a href="/mini-events/event2">Value</a>
-                            </li>
-                            <li>
-                                <a href="/mini-events/event3">Value</a>
-                            </li>
-                        </ul>
-                    </div>
-                </li>
-                <li class="page">
-                    <a href="/campfire-games">Campfire Games</a>
-                </li>
-                <li class="page">
-                    <a href="/schedule">Schedule</a>
-                </li>
-                <li class="page">
-                    <a href="/faq">FAQ</a>
-                </li>
-                <li class="page">
-                    <a href="/sponsors">Sponsors</a>
-                </li>
-            </ul>
-
-        </nav>
-        <div class="divider">
+  <header>
+    <nav>
+      <div class="non-pages">
+        <div id="logo-container">
+          <a tag="img" href="/">
+            <img
+              id="logo-with-text"
+              src="~/public/bitcamp-brand/logos/logotype.png"
+            />
+            <img
+              id="logo-image"
+              src="~/public/bitcamp-brand/logos/bitcamp.png"
+            />
+          </a>
         </div>
+        <div class="hamburgerContainer">
+          <div class="hamburgerBars" @click="toggleDropdown">
+            <div class="bar1"></div>
+            <div class="bar2"></div>
+            <div class="bar3"></div>
+          </div>
+        </div>
+      </div>
 
-    </header>
+      <ul v-if="showDropdown || bigScreen" class="nav-pages">
+        <li class="page">
+          <a href="/our-mission">Our Mission</a>
+        </li>
+        <li class="page">
+          <a href="/tracks">Tracks</a>
+        </li>
+        <li class="page">
+          <a href="/mini-events">Mini Events</a>
+        </li>
+        <li class="page">
+          <a href="/campfire-games">Campfire Games</a>
+        </li>
+        <li class="page">
+          <a href="/schedule">Schedule</a>
+        </li>
+        <li class="page">
+          <a href="/faq">FAQ</a>
+        </li>
+        <li class="page">
+          <a href="/sponsor">Sponsors</a>
+        </li>
+        <template v-if="bigScreen">
+          <li class="image">
+            <a tag="img" href="/">
+              <img id="marshies" src="~/assets/images/rider.png" />
+            </a>
+          </li>
+          <li class="image">
+            <a tag="img" href="/">
+              <img id="marshies" src="~/assets/images/hiker.png" />
+            </a>
+          </li>
+          <li class="image">
+            <a tag="img" href="/">
+              <img id="marshies" src="~/assets/images/watcher.png" />
+            </a>
+          </li>
+          <a
+            id="mlh-trust-badge"
+            style="
+              display: block;
+              height: 32px;
+              width: 32px;
+              max-width: 100px;
+              min-width: 60px;
+              width: 10%;
+              top: 0;
+            "
+            href="https://mlh.io/na?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2023-season&utm_content=blue"
+            target="_blank"
+            ><img
+              src="https://s3.amazonaws.com/logged-assets/trust-badge/2023/mlh-trust-badge-2023-blue.svg"
+              alt="Major League Hacking 2023 Hackathon Season"
+              style="width: 100%"
+          /></a>
+        </template>
+      </ul>
+    </nav>
+  </header>
 
-
-
-    <div>
-        <slot />
-    </div>
-
+  <div>
+    <slot />
+  </div>
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
+import { ref, onMounted } from 'vue';
 
 const showDropdown = ref(false);
 const bigScreen = ref(false);
 
 onMounted(() => {
-    const startSize = window.innerWidth;
-    if (startSize > 992) {
-        bigScreen.value = true;
-    } else {
-        bigScreen.value = false;
-    }
-    window.addEventListener("resize", onresize);
-})
+  const startSize = window.innerWidth;
+  if (startSize > 992) {
+    bigScreen.value = true;
+  } else {
+    bigScreen.value = false;
+  }
+  window.addEventListener('resize', onresize);
+});
 
 onUnmounted(() => {
-    window.removeEventListener("resize", onresize);
-})
+  window.removeEventListener('resize', onresize);
+});
 
 function onresize() {
-    const screenSize = window.innerWidth;
+  const screenSize = window.innerWidth;
 
-    if (screenSize <= 992) {
-        bigScreen.value = false;
-    } else {
-        bigScreen.value = true;
-        showDropdown.value = false;
-    }
+  if (screenSize <= 992) {
+    bigScreen.value = false;
+  } else {
+    bigScreen.value = true;
+    showDropdown.value = false;
+  }
 }
 
 function toggleDropdown() {
-    showDropdown.value = !showDropdown.value;
+  showDropdown.value = !showDropdown.value;
 }
 </script>
 
@@ -112,243 +130,253 @@ $bitcamp: var(--color-bitcamp);
 $mango: var(--color-mango);
 
 header {
-    margin: 0;
-    padding: 0;
-    border: 0;
-    width: 100%;
-    background-color: white;
+  margin: 0;
+  padding: 1.5%;
+  border: 0;
+  width: 100%;
+  background-color: #10274f;
 }
 
 nav {
-    position: relative;
-    margin: 1vw;
-    font-size: 1.25vw;
-    font-family: Aleo;
+  position: relative;
+  margin: 0vw;
+  font-size: 1.25vw;
+  font-family: Aleo;
 }
 
 #logo-container {
-    position: absolute;
-    margin-left: 1%;
-    height: 100%;
-    width: 100%;
+  position: absolute;
+  margin-left: 1%;
+  height: 100%;
+  width: 100%;
+}
+
+#marshie-container {
+  position: absolute;
+  margin-left: 80%;
+  height: 100%;
+  width: 100%;
 }
 
 #logo-with-text {
-    max-height: 100%;
-    max-width: 100%;
-    object-fit: cover;
+  max-height: 100%;
+  max-width: 100%;
+  object-fit: cover;
 }
 
 #logo-image {
-    display: none;
+  display: none;
+}
+
+#marshies {
+  display: flex;
+  height: 32px;
+  width: 32px;
 }
 
 .nav-pages {
-    margin-left: 15vw;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-around;
+  margin-left: 15vw;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
 }
 
 .nav-pages li {
-    display: flex;
-    align-items: center;
-    text-decoration: none !important;
-    z-index: 3;
+  display: flex;
+  align-items: center;
+  text-decoration: none !important;
+  z-index: 3;
 
-    :hover {
-        color: $mango;
-        text-decoration: underline;
-    }
+  :hover {
+    color: $mango;
+    text-decoration: underline;
+  }
 }
 
 .nav-pages li a {
-    color: black;
-    text-decoration: none;
-    font-size: 1.2rem;
+  color: white;
+  text-decoration: none;
+  font-size: 1.2rem;
 }
 
 .divider-large-screen {
-    background-image: linear-gradient(180deg, lightgray, white);
-    position: relative;
-    padding: 0.2%;
-    width: auto;
+  background-image: linear-gradient(180deg, lightgray, white);
+  position: relative;
+  padding: 0.2%;
+  width: auto;
 }
 
 .dropdown-page {
-    display: flex;
-    flex-direction: column;
-    width: auto;
-    align-content: flex-start;
+  display: flex;
+  flex-direction: column;
+  width: auto;
+  align-content: flex-start;
 }
 
-
 .dropdown-page:hover .dropdown-elements-container {
-    display: flex;
+  display: flex;
 }
 
 .dropdown-elements-container {
-    display: none;
-    flex-direction: column;
-    align-self: flex-start;
-    width: 100%;
-    position: relative;
-    background-color: white;
-    text-decoration: none !important;
+  display: none;
+  flex-direction: column;
+  align-self: flex-start;
+  width: 100%;
+  position: relative;
+  background-color: white;
+  text-decoration: none !important;
 }
 
 .dropdown-elements {
-    display: flex;
-    flex-direction: column;
-    align-self: center;
-    align-items: flex-end;
-    position: absolute;
-    border: 0.1vw solid gray;
-    border-radius: 10%;
-    z-index: 1;
-    background: white;
-    height: fit-content;
-    font-family: Avenir;
-    width: 130%;
-    position: absolute;
-    text-decoration: none !important;
+  display: flex;
+  flex-direction: column;
+  align-self: center;
+  align-items: flex-end;
+  position: absolute;
+  border: 0.1vw solid gray;
+  border-radius: 10%;
+  z-index: 1;
+  background: white;
+  height: fit-content;
+  font-family: Avenir;
+  width: 130%;
+  position: absolute;
+  text-decoration: none !important;
 }
 
 .dropdown-page:hover .dropdown-text {
-    color: $mango;
-    text-decoration: underline;
+  color: $mango;
+  text-decoration: underline;
 }
 
 .dropdown-elements li {
-    display: flex;
-    flex-direction: column;
-    text-align: left;
-    position: relative;
-    width: 90%;
-    font-size: 0.6rem;
-    margin: 4%;
-
+  display: flex;
+  flex-direction: column;
+  text-align: left;
+  position: relative;
+  width: 90%;
+  font-size: 0.6rem;
+  margin: 4%;
 }
 
 .dropdown-elements li a {
-    width: 100%;
-    padding: 2%;
-    margin-left: 5%;
+  width: 100%;
+  padding: 2%;
+  margin-left: 5%;
 }
 
 .dropdown-elements li:hover a {
-    color: $bitcamp;
-    border-radius: 0.5rem;
-    width: 100%;
-    text-decoration: none !important;
+  color: $bitcamp;
+  border-radius: 0.5rem;
+  width: 100%;
+  text-decoration: none !important;
 
-    &::before {
-        content: '';
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        background-color: $bitcamp;
-        opacity: 0.3;
-        border-radius: 0.5rem;
-        z-index: -1;
-    }
+  &::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: $bitcamp;
+    opacity: 0.3;
+    border-radius: 0.5rem;
+    z-index: -1;
+  }
 }
 
 .hamburgerContainer {
-    display: none;
+  display: flex;
 }
 
 .divider {
-    background-image: linear-gradient(180deg, lightgray, white);
-    position: relative;
-    padding: 0.2%;
-    width: auto;
+  background-image: linear-gradient(180deg, lightgray, white);
+  position: relative;
+  padding: 0.2%;
+  width: auto;
 }
 
 @media only screen and (max-width: 992px) {
+  nav {
+    display: flex;
+    flex-direction: column;
+    align-self: flex-start;
+    margin: 0;
+  }
 
-    nav {
-        display: flex;
-        flex-direction: column;
-        align-self: flex-start;
-        margin: 0;
-    }
+  non-pages {
+    display: flex;
+    flex-direction: row;
+  }
 
-    non-pages {
-        display: flex;
-        flex-direction: row;
-    }
+  #logo-with-text {
+    display: none;
+    position: relative;
+  }
 
-    #logo-with-text {
-        display: none;
-        position: relative;
-    }
+  #logo-image {
+    display: block;
+    max-width: 100%;
+    max-height: 5vw;
+  }
 
-    #logo-image {
-        display: block;
-        max-width: 100%;
-        max-height: 5vw;
-    }
+  .nav-pages {
+    margin-left: 1vw;
+    width: 100%;
+    font-size: 32px;
+    background-color: #10274f;
+    position: relative;
+    flex-direction: column;
+    align-self: flex-start;
+    align-items: center;
+    padding-bottom: 5%;
+  }
 
-    .nav-pages {
-        margin-left: 1vw;
-        width: 100%;
-        background-color: white;
-        position: relative;
-        flex-direction: column;
-        align-self: flex-start;
-        align-items: flex-start;
-    }
+  .nav-pages li {
+    margin-top: 1vw;
+  }
 
-    .nav-pages li {
-        margin-top: 1vw;
-    }
+  .hamburgerContainer {
+    position: relative;
+    display: flex;
+    margin-right: 1vw;
+    align-items: flex-end;
+    flex-direction: column;
+  }
 
-    .hamburgerContainer {
-        position: relative;
-        display: flex;
-        margin-right: 1vw;
-        align-items: flex-end;
-        flex-direction: column;
-    }
+  .hamburgerBars {
+    display: flex;
+    flex-direction: column;
+    cursor: pointer;
+  }
 
-    .hamburgerBars {
-        display: flex;
-        flex-direction: column;
-        cursor: pointer;
-    }
+  .dropdown-elements {
+    align-self: flex-start;
+    margin-left: 2vw;
+    position: relative;
+    border: none;
+  }
 
-    .dropdown-elements {
-        align-self: flex-start;
-        margin-left: 2vw;
-        position: relative;
-        border: none;
-    }
+  .dropdown-elements li {
+    margin: 2%;
+  }
 
-    .dropdown-elements li {
-        margin: 2%;
-    }
+  .dropdown-elements li:hover a {
+    background-color: white;
+  }
 
-    .dropdown-elements li:hover a {
-        background-color: white;
-    }
+  .bar1,
+  .bar2,
+  .bar3 {
+    width: 5vw;
+    height: 0.75vw;
+    margin: 0.5vw 0vw;
+    border-radius: 1rem;
+    background-color: $bitcamp;
+  }
 
-    .bar1,
-    .bar2,
-    .bar3 {
-        width: 5vw;
-        height: 0.75vw;
-        margin: 0.5vw 0vw;
-        border-radius: 1rem;
-        background-color: $bitcamp;
-    }
-
-    .divider {
-        display: none;
-    }
-
+  .divider {
+    display: none;
+  }
 }
 </style>
-
