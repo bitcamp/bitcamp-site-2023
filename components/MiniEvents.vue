82a81a39df7cb68803901212c6e45ff8f2c7ceec
Eric Chen <ericchen314@gmail.com>
2023-02-22 01:29:07 -0500

    Add MiniEvents background blur component

--- components/MiniEvents.vue
+++ components/MiniEvents.vue
@@ -1,30 +1,39 @@
 <template>
-  <h1 id="mini-events">Mini-Events</h1>
+  <div id="mini-events">
+    <h1 id="mini-events-title">Mini-Events</h1>
 
-  <div class="mini-events-container">
-    <article class="card">
-      <img src="~/assets/svg/color-war.svg" alt="Color War Photo" />
-      <div class="text">
-        <h1>Color War</h1>
-        <p>
-          Colorwar is a rapid-fire live-design competition and one of the
-          highlights of Bitcamp. Compete in our online design challenge for a
-          chance to draw in front of a live audience at Bitcamp for some cool
-          prizes!
-        </p>
-      </div>
-    </article>
-    <article class="card">
-      <img src="~/assets/svg/design-den.svg" alt="Design Den Photo" />
-      <div class="text">
-        <h1>Design Den</h1>
-        <p>
-          Need help designing your hack during Bitcamp? Stop by Design Den, and
-          Bitcamp’s design team will help you work on your hack’s user interface
-          and user experience. It’s like office hours, but for design 😎
-        </p>
-      </div>
-    </article>
+    <div class="mini-events-container">
+      <article class="card">
+        <img src="~/assets/svg/color-war.svg" alt="Color War Photo" />
+        <div class="text">
+          <h1>Color War</h1>
+          <p>
+            Colorwar is a rapid-fire live-design competition and one of the
+            highlights of Bitcamp. Compete in our online design challenge for a
+            chance to draw in front of a live audience at Bitcamp for some cool
+            prizes!
+          </p>
+        </div>
+        <span class="button-wrap">
+        <Button text="Learn More" link="https://bitcmp.medium.com/calling-all-creatives-for-colorwar-443f19759cc2" size="xl" color="#324A79" />
+        </span>
+      </article>
+      <article class="card">
+        <img src="~/assets/svg/design-den.svg" alt="Design Den Photo" />
+        <div class="text">
+          <h1>Design Den</h1>
+          <p>
+            Need help designing your hack during Bitcamp? Stop by Design Den,
+            and Bitcamp’s design team will help you work on your hack’s user
+            interface and user experience. It’s like office hours, but for
+            design 😎
+          </p>
+        </div>
+        <span class="button-wrap">
+        <Button text="Watch on YouTube" link="https://www.youtube.com/watch?v=jjP1iaQka9Y&ab_channel=Bitcamp" size="xl" color="#324A79" />
+        </span>
+      </article>
+    </div>
   </div>
 </template>
 
@@ -33,16 +42,17 @@
 <style scoped lang="scss">
 #mini-events {
   text-align: center;
-  margin-top: 5rem;
-  margin-bottom: 3rem;
-}
-.mini-events-container {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  align-items: stretch;
+  box-sizing: border-box;
+  background-color: #21498e80;
+  backdrop-filter: blur(35px);
+  border-radius: 10px;
+  color: white;
+  box-shadow: 0 0.5rem 1rem rgba(black, 0.3);
   width: 70%;
   margin: 0 auto;
+  padding-top: 4rem;
+  padding-bottom: 4rem;
+  margin-top: 5rem;
 
   @media (min-width: 576px) and (max-width: 767.8px) {
     width: 90%;
@@ -51,6 +61,15 @@
   @media (max-width: 576px) {
     width: 90%;
   }
+}
+
+.mini-events-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: stretch;
+  
+  margin: 0 auto;
 }
 .card {
   display: flex;
@@ -64,16 +83,24 @@
 
   img {
     max-width: 100%;
-    margin-bottom: 10px;
+    margin-bottom: 15px;
   }
 
   .text {
-    margin: 10px;
+    margin: 5px;
     flex: 1;
   }
 
+  .button-wrap {
+    margin-top: 2rem;
+  }
+
   .text h1 {
-    margin-bottom: 10px;
+    margin-bottom: 15px;
+  }
+
+  Button {
+    margin-top: 100px;
   }
 
   @media (min-width: 576px) and (max-width: 767.8px) {
