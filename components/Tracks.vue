0fcadf55c5ae513d409b22278ce8b8cedb1db643
Eric Chen <ericchen314@gmail.com>
2023-02-22 01:30:14 -0500

    Change tracks id organization

--- components/Tracks.vue
+++ components/Tracks.vue
@@ -1,73 +1,75 @@
 <template>
-  <h1 id="tracks">Tracks</h1>
+  <div id="tracks">
+    <h1 id="tracks-title">Tracks</h1>
 
-  <div class="tracks-container">
-    <article class="card">
-      <img src="~/assets/svg/general.svg" alt="General Track Photo" />
-      <div class="text">
-        <h1>General Track</h1>
-        <p>
-          For any and all hackers! Build the perfect hack using hardware,
-          software, and collaboration with other tech-lovers, design thinkers,
-          and students - all skill and experience levels are welcome!
-        </p>
-      </div>
-    </article>
-    <article class="card">
-      <img src="~/assets/svg/beginner.svg" alt="Beginner Track Photo" />
-      <div class="text">
-        <h1>Beginner Track</h1>
-        <p>
-          New to hackathons? Don't know how to code? No worries! At Bitcamp,
-          you'll have access to beginner-friendly workshops, helpful mentors,
-          personalized one-on-one sessions, and resources to kickstart your
-          coding journey.
-        </p>
-      </div>
-    </article>
-    <article class="card">
-      <img src="~/assets/svg/hardware.svg" alt="Hardware Track Photo" />
-      <div class="text">
-        <h1>Hardware Track</h1>
-        <p>
-          Embark on your hardware journey at Bitcamp with access to exclusive
-          hardware materials, mentors, and workshops! We'll provide all the
-          resources needed to build your very own hardware project and master
-          hardware fundamentals.
-        </p>
-      </div>
-    </article>
-    <article class="card">
-      <img src="~/assets/svg/quantum.svg" alt="Quantum Track Photo" />
-      <div class="text">
-        <h1>Quantum Track</h1>
-        <p>
-          Hackers will delve into the field of quantum computing with exclusive
-          mentors, sponsors, and workshops! Hackers will use their knowledge of
-          Python and other computing skills on educational and interactive
-          Quantum Track activities.
-        </p>
-      </div>
-    </article>
-    <article class="card">
-      <img src="~/assets/svg/ml.svg" alt="Machine Learning Track Photo" />
-      <div class="text">
-        <h1>Machine Learning Track</h1>
-        <p>
-          Our newest track! If you’re wowed by recent AI advancements like
-          ChatGPT, this is the track for you! We will have ML-related workshops,
-          guest speakers, and activities (basic programming skills highly
-          recommended).
-        </p>
-      </div>
-    </article>
+    <div class="tracks-container">
+      <article class="card">
+        <img src="~/assets/svg/general.svg" alt="General Track Photo" />
+        <div class="text">
+          <h1>General Track</h1>
+          <p>
+            For any and all hackers! Build the perfect hack using hardware,
+            software, and collaboration with other tech-lovers, design thinkers,
+            and students - all skill and experience levels are welcome!
+          </p>
+        </div>
+      </article>
+      <article class="card">
+        <img src="~/assets/svg/beginner.svg" alt="Beginner Track Photo" />
+        <div class="text">
+          <h1>Beginner Track</h1>
+          <p>
+            New to hackathons? Don't know how to code? No worries! At Bitcamp,
+            you'll have access to beginner-friendly workshops, helpful mentors,
+            personalized one-on-one sessions, and resources to kickstart your
+            coding journey.
+          </p>
+        </div>
+      </article>
+      <article class="card">
+        <img src="~/assets/svg/hardware.svg" alt="Hardware Track Photo" />
+        <div class="text">
+          <h1>Hardware Track</h1>
+          <p>
+            Embark on your hardware journey at Bitcamp with access to exclusive
+            hardware materials, mentors, and workshops! We'll provide all the
+            resources needed to build your very own hardware project and master
+            hardware fundamentals.
+          </p>
+        </div>
+      </article>
+      <article class="card">
+        <img src="~/assets/svg/quantum.svg" alt="Quantum Track Photo" />
+        <div class="text">
+          <h1>Quantum Track</h1>
+          <p>
+            Hackers will delve into the field of quantum computing with
+            exclusive mentors, sponsors, and workshops! Hackers will use their
+            knowledge of Python and other computing skills on educational and
+            interactive Quantum Track activities.
+          </p>
+        </div>
+      </article>
+      <article class="card">
+        <img src="~/assets/svg/ml.svg" alt="Machine Learning Track Photo" />
+        <div class="text">
+          <h1>Machine Learning Track</h1>
+          <p>
+            Our newest track! If you’re wowed by recent AI advancements like
+            ChatGPT, this is the track for you! We will have ML-related
+            workshops, guest speakers, and activities (basic programming skills
+            highly recommended).
+          </p>
+        </div>
+      </article>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts"></script>
 
 <style scoped lang="scss">
-#tracks {
+#tracks-title {
   text-align: center;
   margin-bottom: 3rem;
 }
@@ -95,20 +97,20 @@
   text-align: center;
   width: calc(33.33% - 30px);
   margin: 15px;
-  margin-bottom: 20px;
+  margin-bottom: 15px;
 
   img {
     max-width: 100%;
-    margin-bottom: 10px;
+    margin-bottom: 20px;
   }
 
   .text {
-    margin: 10px;
+    margin: 5px;
     flex: 1;
   }
 
   .text h1 {
-    margin-bottom: 10px;
+    margin-bottom: 15px;
   }
 
   @media (min-width: 576px) and (max-width: 767.8px) {
