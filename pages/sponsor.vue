33000ee1ffa0e4934ec48d87c1930255d89c6158
Varun Chilukuri <varunch321@gmail.com>
2023-02-28 16:55:37 -0500

    Add meta tags

--- pages/sponsor.vue
+++ pages/sponsor.vue
@@ -582,6 +582,96 @@
   </div>
 </template>
 
+
+<script setup lang="ts">
+useHead({
+  title: 'Bitcamp | Sponsor Prospectus',
+  meta: [
+    {
+      name: 'description',
+      content: "",
+    },
+    {
+      property: 'og:title',
+      content: 'Bitcamp | Sponsor Prospectus',
+    },
+    {
+      property: 'og:site_name',
+      content: 'Bitcamp',
+    },
+    {
+      property: 'og:url',
+      content: 'https://bit.camp/sponsor',
+    },
+    {
+      property: 'og:description',
+      content:
+      "",
+    },
+    {
+      property: 'og:type',
+      content: 'website',
+    },
+    {
+      property: 'twitter:card',
+      content: 'summary',
+    },
+    {
+      property: 'twitter:url',
+      content: 'https://bit.camp/sponsor',
+    },
+    {
+      property: 'twitter:title',
+      content: 'Bitcamp | Sponsor Prospectus',
+    },
+    {
+      property: 'twitter:description',
+      content:
+      "",
+    },
+    {
+      name: 'msapplication-TileColor',
+      content: '#ff6f3f',
+    },
+    {
+      name: 'msapplication-config',
+      content: '/bitcamp-brand/favicons/browserconfig.xml',
+    },
+    {
+      name: 'theme-color',
+      content: '#ffffff',
+    },
+  ],
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '32x32',
+      href: '/bitcamp-brand/favicons/favicon-32x32.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '16x16',
+      href: '/bitcamp-brand/favicons/favicon-16x16.png',
+    },
+    {
+      rel: 'manifest',
+      href: '/bitcamp-brand/favicons/site.webmanifest',
+    },
+    {
+      rel: 'mask-icon',
+      href: '/bitcamp-brand/favicons/safari-pinned-tab.svg',
+      color: '#ff6f3f',
+    },
+    {
+      rel: 'shortcut icon',
+      href: '/bitcamp-brand/favicons/favicon.ico',
+    },
+  ],
+});
+</script>
+
 <script lang="ts">
 export default {
   name: 'SponsorPage',
