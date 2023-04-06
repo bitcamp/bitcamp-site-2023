de59101c8f467440e0de21d23e9199cf1b288ac5
Jason Liu <jasoneliu03@gmail.com>
2023-04-06 11:47:50 -0400

    Add schedule page

---
+++ pages/schedule.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="schedule-page">
+    <Schedule />
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'SchedulePage',
+};
+</script>
+
+<style lang="scss">
+.schedule-page {
+  display: flex;
+  align-items: center;
+  background-color: #345793;
+  height: 100dvh;
+}
+</style>
